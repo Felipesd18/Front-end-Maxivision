114fb8f4c27384f8f91c02b935973aa7c376f7a3
Felipe Solís <felipe.solis@usach.cl>
2022-09-23 19:54:15 -0300

    Creación formulario Agregar Cristal con sus diseños

---
+++ pages/AgregarCristal.vue
@@ -0,0 +1,746 @@
+<template>
+  <div class="agregar-cristal-container">
+    <sidebar-menu />
+    <div class="agregar-cristal-agregar-cristal">
+      <span class="agregar-cristal-text">
+        <span>Ingreso Cristal a Inventario</span>
+      </span>
+      <span class="agregar-cristal-text02"><span>Tipo</span></span>
+      <div class="agregar-cristal-send-box">
+        <span class="agregar-cristal-text04"><span>Ingresar</span></span>
+      </div>
+      <div class="agregar-cristal-tabla-a-d-d">
+        <img
+          src="/playground_assets/rectangle234623-tisu-200h.png"
+          alt="Rectangle234623"
+          class="agregar-cristal-rectangle23"
+        />
+        <img
+          src="/playground_assets/rectangle244623-rda-200h.png"
+          alt="Rectangle244623"
+          class="agregar-cristal-rectangle24"
+        />
+        <img
+          src="/playground_assets/rectangle254624-rzvb-200h.png"
+          alt="Rectangle254624"
+          class="agregar-cristal-rectangle25"
+        />
+        <span class="agregar-cristal-text06"><span>ADD</span></span>
+        <span class="agregar-cristal-text08"><span>Input</span></span>
+        <span class="agregar-cristal-text10"><span>Input</span></span>
+      </div>
+      <div class="agregar-cristal-tabla">
+        <img
+          src="/playground_assets/rectangle24628-rgpa-200h.png"
+          alt="Rectangle24628"
+          class="agregar-cristal-rectangle2"
+        />
+        <img
+          src="/playground_assets/rectangle224613-e0nm-200h.png"
+          alt="Rectangle224613"
+          class="agregar-cristal-rectangle22"
+        />
+        <img
+          src="/playground_assets/rectangle34634-sqa6-200h.png"
+          alt="Rectangle34634"
+          class="agregar-cristal-rectangle3"
+        />
+        <img
+          src="/playground_assets/rectangle44635-gemi-200h.png"
+          alt="Rectangle44635"
+          class="agregar-cristal-rectangle4"
+        />
+        <img
+          src="/playground_assets/rectangle54636-weke-200h.png"
+          alt="Rectangle54636"
+          class="agregar-cristal-rectangle5"
+        />
+        <img
+          src="/playground_assets/rectangle64637-fih-200h.png"
+          alt="Rectangle64637"
+          class="agregar-cristal-rectangle6"
+        />
+        <img
+          src="/playground_assets/rectangle74638-c5kn-200h.png"
+          alt="Rectangle74638"
+          class="agregar-cristal-rectangle7"
+        />
+        <img
+          src="/playground_assets/rectangle84639-adiw-200h.png"
+          alt="Rectangle84639"
+          class="agregar-cristal-rectangle8"
+        />
+        <img
+          src="/playground_assets/rectangle104641-x62o-200h.png"
+          alt="Rectangle104641"
+          class="agregar-cristal-rectangle10"
+        />
+        <img
+          src="/playground_assets/rectangle124643-uc78-200h.png"
+          alt="Rectangle124643"
+          class="agregar-cristal-rectangle12"
+        />
+        <img
+          src="/playground_assets/rectangle144645-tqa-200h.png"
+          alt="Rectangle144645"
+          class="agregar-cristal-rectangle14"
+        />
+        <img
+          src="/playground_assets/rectangle164648-hlkg-200h.png"
+          alt="Rectangle164648"
+          class="agregar-cristal-rectangle16"
+        />
+        <img
+          src="/playground_assets/rectangle184650-i7z-200h.png"
+          alt="Rectangle184650"
+          class="agregar-cristal-rectangle18"
+        />
+        <img
+          src="/playground_assets/rectangle204652-iaw-200h.png"
+          alt="Rectangle204652"
+          class="agregar-cristal-rectangle20"
+        />
+        <span class="agregar-cristal-text12"><span>Esfera</span></span>
+        <span class="agregar-cristal-text14"><span>Cilindro</span></span>
+        <span class="agregar-cristal-text16"><span>Eje</span></span>
+        <span class="agregar-cristal-text18"><span>DP</span></span>
+        <span class="agregar-cristal-text20"><span>CR o MIN</span></span>
+        <span class="agregar-cristal-text22"><span>FOTO o AR</span></span>
+        <span class="agregar-cristal-text24"><span>OD</span></span>
+        <span class="agregar-cristal-text26"><span>Input</span></span>
+        <span class="agregar-cristal-text28"><span>Input</span></span>
+        <span class="agregar-cristal-text30"><span>Input</span></span>
+        <span class="agregar-cristal-text32"><span>Input</span></span>
+        <span class="agregar-cristal-text34"><span>Input</span></span>
+        <span class="agregar-cristal-text36"><span>Input</span></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AgregarCristal',
+  head: {
+    title: 'exported project',
+  },
+}
+</script>
+
+<style scoped>
+.agregar-cristal-container {
+  min-height: 100vh;
+}
+.agregar-cristal-agregar-cristal {
+  width: 100%;
+  height: 1080px;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+  max-width: 1920px;
+  box-sizing: border-box;
+  align-items: flex-start;
+  flex-shrink: 0;
+  border-color: transparent;
+  border-radius: 0px 0px 0px 0px;
+  background-color: rgba(255, 255, 255, 1);
+}
+.agregar-cristal-text {
+  top: 14px;
+  left: 20px;
+  color: rgba(0, 0, 0, 1);
+  width: 900px;
+  height: auto;
+  position: absolute;
+  font-size: 64px;
+  align-self: auto;
+  font-style: Bold;
+  text-align: left;
+  font-family: Poppins;
+  font-weight: 700;
+  line-height: normal;
+  font-stretch: normal;
+  margin-right: 0;
+  margin-bottom: 0;
+  text-decoration: none;
+}
+.agregar-cristal-text02 {
+  top: 152px;
+  left: 20px;
+  color: rgba(0, 0, 0, 1);
+  width: 60px;
+  height: auto;
+  position: absolute;
+  font-size: 24px;
+  align-self: auto;
+  font-style: Bold;
+  text-align: left;
+  font-family: Poppins;
+  font-weight: 700;
+  line-height: normal;
+  font-stretch: normal;
+  margin-right: 0;
+  margin-bottom: 0;
+  text-decoration: none;
+}
+.agregar-cristal-send-box {
+  top: 658px;
+  left: 1582.5px;
+  width: 218px;
+  height: 83px;
+  display: flex;
+  overflow: hidden;
+  position: absolute;
+  box-sizing: border-box;
+  align-items: flex-start;
+  flex-shrink: 0;
+  border-color: transparent;
+  margin-right: 0;
+  border-radius: 15px;
+  margin-bottom: 0;
+  background-color: rgba(17, 16, 29, 1);
+}
+.agregar-cristal-text04 {
+  top: 16px;
+  left: 35px;
+  color: rgba(255, 255, 255, 1);
+  width: 147px;
+  height: auto;
+  position: absolute;
+  font-size: 32px;
+  align-self: auto;
+  font-style: Regular;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 400;
+  line-height: normal;
+  font-stretch: normal;
+  margin-right: 0;
+  margin-bottom: 0;
+  text-decoration: none;
+}
+.agregar-cristal-tabla-a-d-d {
+  top: 537px;
+  left: 20px;
+  width: 1203px;
+  height: 77px;
+  display: flex;
+  padding: 0;
+  position: absolute;
+  align-self: auto;
+  box-sizing: border-box;
+  align-items: flex-start;
+  flex-shrink: 1;
+  border-color: transparent;
+  border-style: none;
+  border-width: 0;
+  margin-right: 0;
+  border-radius: 0px 0px 0px 0px;
+  margin-bottom: 0;
+  flex-direction: row;
+  justify-content: flex-start;
+  background-color: transparent;
+}
+.agregar-cristal-rectangle23 {
+  top: 0px;
+  left: 0px;
+  width: 192px;
+  height: 77px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(17, 16, 29, 1);
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 15px 0 0 15px;
+}
+.agregar-cristal-rectangle24 {
+  top: 0px;
+  left: 191px;
+  width: 763px;
+  height: 77px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(17, 16, 29, 1);
+  border-style: solid;
+  border-width: 1px;
+}
+.agregar-cristal-rectangle25 {
+  top: 0px;
+  left: 953px;
+  width: 250px;
+  height: 77px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(17, 16, 29, 1);
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 0 15px 15px 0;
+}
+.agregar-cristal-text06 {
+  top: 1px;
+  left: 1px;
+  color: rgba(0, 0, 0, 1);
+  width: 190px;
+  bottom: 1px;
+  height: auto;
+  position: absolute;
+  font-size: 36px;
+  align-self: auto;
+  font-style: ExtraLight;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 200;
+  line-height: 2;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text08 {
+  top: 1px;
+  left: 192px;
+  color: rgba(0, 0, 0, 1);
+  width: 761px;
+  bottom: 1px;
+  height: auto;
+  position: absolute;
+  font-size: 32px;
+  align-self: auto;
+  font-style: Thin;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 100;
+  line-height: 2;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text10 {
+  left: 954px;
+  color: rgba(0, 0, 0, 1);
+  width: 248px;
+  bottom: 1px;
+  height: auto;
+  position: absolute;
+  font-size: 32px;
+  align-self: auto;
+  font-style: Thin;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 100;
+  line-height: 2;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-tabla {
+  top: 362px;
+  left: 20px;
+  width: 1680px;
+  height: 136px;
+  display: flex;
+  padding: 0;
+  position: absolute;
+  align-self: auto;
+  box-sizing: border-box;
+  align-items: flex-start;
+  flex-shrink: 1;
+  border-color: transparent;
+  border-style: none;
+  border-width: 0;
+  margin-right: 0;
+  border-radius: 0px 0px 0px 0px;
+  margin-bottom: 0;
+  flex-direction: row;
+  justify-content: flex-start;
+  background-color: transparent;
+}
+.agregar-cristal-rectangle2 {
+  top: 0px;
+  left: 189px;
+  width: 255px;
+  height: 59px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(17, 16, 29, 1);
+  border-style: solid;
+  border-width: 1px;
+}
+.agregar-cristal-rectangle22 {
+  top: 0px;
+  left: 0px;
+  width: 190px;
+  height: 59px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(17, 16, 29, 1);
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 15px 0 0;
+}
+.agregar-cristal-rectangle3 {
+  top: 0px;
+  left: 443px;
+  width: 255px;
+  height: 59px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(17, 16, 29, 1);
+  border-style: solid;
+  border-width: 1px;
+}
+.agregar-cristal-rectangle4 {
+  top: 0px;
+  left: 697px;
+  width: 255px;
+  height: 59px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(17, 16, 29, 1);
+  border-style: solid;
+  border-width: 1px;
+}
+.agregar-cristal-rectangle5 {
+  top: 0px;
+  left: 951px;
+  width: 255px;
+  height: 59px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(17, 16, 29, 1);
+  border-style: solid;
+  border-width: 1px;
+}
+.agregar-cristal-rectangle6 {
+  top: 0px;
+  left: 1205px;
+  width: 255px;
+  height: 59px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(17, 16, 29, 1);
+  border-style: solid;
+  border-width: 1px;
+}
+.agregar-cristal-rectangle7 {
+  top: 0px;
+  left: 1458px;
+  width: 222px;
+  height: 59px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(17, 16, 29, 1);
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 0 15px 0 0;
+}
+.agregar-cristal-rectangle8 {
+  top: 58px;
+  left: 0px;
+  width: 190px;
+  height: 78px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(17, 16, 29, 1);
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 0 0 0 15px;
+}
+.agregar-cristal-rectangle10 {
+  top: 58px;
+  left: 189px;
+  width: 255px;
+  height: 78px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(0, 0, 0, 1);
+  border-style: solid;
+  border-width: 1px;
+}
+.agregar-cristal-rectangle12 {
+  top: 58px;
+  left: 443px;
+  width: 255px;
+  height: 78px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(0, 0, 0, 1);
+  border-style: solid;
+  border-width: 1px;
+}
+.agregar-cristal-rectangle14 {
+  top: 58px;
+  left: 697px;
+  width: 255px;
+  height: 78px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(0, 0, 0, 1);
+  border-style: solid;
+  border-width: 1px;
+}
+.agregar-cristal-rectangle16 {
+  top: 58px;
+  left: 951px;
+  width: 255px;
+  height: 78px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(0, 0, 0, 1);
+  border-style: solid;
+  border-width: 1px;
+}
+.agregar-cristal-rectangle18 {
+  top: 58px;
+  left: 1205px;
+  width: 255px;
+  height: 78px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(0, 0, 0, 1);
+  border-style: solid;
+  border-width: 1px;
+}
+.agregar-cristal-rectangle20 {
+  top: 58px;
+  left: 1458px;
+  width: 222px;
+  height: 78px;
+  position: absolute;
+  box-sizing: border-box;
+  border-color: rgba(0, 0, 0, 1);
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 0 0 15px;
+}
+.agregar-cristal-text12 {
+  top: 1px;
+  left: 190px;
+  color: rgba(0, 0, 0, 1);
+  width: 253px;
+  bottom: 77px;
+  height: auto;
+  position: absolute;
+  font-size: 36px;
+  align-self: auto;
+  font-style: ExtraLight;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 200;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text14 {
+  top: 1px;
+  left: 444px;
+  color: rgba(0, 0, 0, 1);
+  width: 253px;
+  bottom: 77px;
+  height: auto;
+  position: absolute;
+  font-size: 36px;
+  align-self: auto;
+  font-style: ExtraLight;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 200;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text16 {
+  top: 1px;
+  left: 698px;
+  color: rgba(0, 0, 0, 1);
+  width: 253px;
+  bottom: 77px;
+  height: auto;
+  position: absolute;
+  font-size: 36px;
+  align-self: auto;
+  font-style: ExtraLight;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 200;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text18 {
+  top: 1px;
+  left: 952px;
+  color: rgba(0, 0, 0, 1);
+  width: 253px;
+  bottom: 77px;
+  height: auto;
+  position: absolute;
+  font-size: 36px;
+  align-self: auto;
+  font-style: ExtraLight;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 200;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text20 {
+  top: 1px;
+  left: 1206px;
+  color: rgba(0, 0, 0, 1);
+  width: 252px;
+  bottom: 77px;
+  height: auto;
+  position: absolute;
+  font-size: 36px;
+  align-self: auto;
+  font-style: ExtraLight;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 200;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text22 {
+  top: 1px;
+  left: 1459px;
+  color: rgba(0, 0, 0, 1);
+  width: 220px;
+  bottom: 77px;
+  height: auto;
+  position: absolute;
+  font-size: 36px;
+  align-self: auto;
+  font-style: ExtraLight;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 200;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text24 {
+  top: 59px;
+  left: 1px;
+  color: rgba(0, 0, 0, 1);
+  width: 188px;
+  bottom: 1px;
+  height: auto;
+  position: absolute;
+  font-size: 36px;
+  align-self: auto;
+  font-style: ExtraLight;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 200;
+  line-height: 2;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text26 {
+  top: 59px;
+  left: 190px;
+  color: rgba(0, 0, 0, 1);
+  width: 253px;
+  bottom: 1px;
+  height: auto;
+  position: absolute;
+  font-size: 32px;
+  align-self: auto;
+  font-style: Thin;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 100;
+  line-height: 2;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text28 {
+  top: 59px;
+  left: 444px;
+  color: rgba(0, 0, 0, 1);
+  width: 253px;
+  bottom: 1px;
+  height: auto;
+  position: absolute;
+  font-size: 32px;
+  align-self: auto;
+  font-style: Thin;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 100;
+  line-height: 2;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text30 {
+  top: 59px;
+  left: 698px;
+  color: rgba(0, 0, 0, 1);
+  width: 253px;
+  bottom: 1px;
+  height: auto;
+  position: absolute;
+  font-size: 32px;
+  align-self: auto;
+  font-style: Thin;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 100;
+  line-height: 2;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text32 {
+  top: 59px;
+  left: 952px;
+  color: rgba(0, 0, 0, 1);
+  width: 253px;
+  bottom: 1px;
+  height: auto;
+  position: absolute;
+  font-size: 32px;
+  align-self: auto;
+  font-style: Thin;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 100;
+  line-height: 2;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text34 {
+  top: 59px;
+  left: 1206px;
+  color: rgba(0, 0, 0, 1);
+  width: 253px;
+  bottom: 1px;
+  height: auto;
+  position: absolute;
+  font-size: 32px;
+  align-self: auto;
+  font-style: Thin;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 100;
+  line-height: 2;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.agregar-cristal-text36 {
+  top: 59px;
+  left: 1459px;
+  color: rgba(0, 0, 0, 1);
+  width: 220px;
+  bottom: 1px;
+  height: auto;
+  position: absolute;
+  font-size: 32px;
+  align-self: auto;
+  font-style: Thin;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: 100;
+  line-height: 2;
+  font-stretch: normal;
+  text-decoration: none;
+}
+</style>
