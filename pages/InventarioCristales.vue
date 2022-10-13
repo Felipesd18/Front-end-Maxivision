93890267814fa58f4d1ea48877d577b7adcd4c37
Carla Polanco <carla.polanco@usach.cl>
2022-10-12 23:12:23 -0300

    Enlistas cristales y marcos listo (falta corregir pagina de marcos)

--- pages/InventarioCristales.vue
+++ pages/InventarioCristales.vue
@@ -23,17 +23,79 @@
         </span>
       </div>
       <span class="inventario-cristales-text08"><span>Listado</span></span>
+
+      <div class= "Contenedor">
+
+        <div class= "Columnas">
+          <span class="Columna1">Tipo</span>
+          <span class="Columna2">Esferico</span>
+          <span class="Columna3">Cilindro</span>
+          <span class="Columna4">Eje</span>
+          <span class="Columna5">DP</span>
+          <span class="Columna6">CR/MIN</span>
+          <span class="Columna7">Foto/Ar</span>
+          <span class="Columna8">ADD</span>
+          <span class="Columna9">Estado</span>
+          <span class="Columna10">Sucursal</span>
+        </div>
+          <div class= "Columnas" v-for= "(cristal,index) in listadoCristales" :key="index">
+            <span class="Columna1">{{cristal.tipo}}</span>
+            <span class="Columna2">{{cristal.esferico}}</span>
+            <span class="Columna3">{{cristal.cilindro}}</span>
+            <span class="Columna4">{{cristal.eje}}</span>
+            <span class="Columna5">{{cristal.dp}}</span>
+            <span class="Columna6">{{cristal.cr_min}}</span>
+            <span class="Columna7">{{cristal.foto_ar}}</span>
+            <span class="Columna8">{{cristal.add}}</span>
+            <span class="Columna9">{{cristal.estado_proceso}}</span>
+            <span class="Columna10" v-for="(sucursal, indice) in listadoSucursales" :key="indice">
+              <span v-if="sucursal.id == cristal.id_sucursal">{{sucursal.nombre}}</span>
+            </span>
+          </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'InventarioCristales',
   head: {
     title: 'exported project',
   },
+
+  data: function () {
+    return {
+      listadoCristales: [],
+      listadoSucursales: [],
+    }
+  },
+
+  methods: {
+    getData: async function () {
+      try {
+
+        let response = await this.$axios.get('/cristal')
+        this.listadoCristales = response.data
+
+        let sucursales = await this.$axios.get('/sucursal')
+        this.listadoSucursales = sucursales.data
+
+
+      } catch (error) {
+        console.log('Error al obtener listado cristales',error)
+      }
+    },
+  },
+
+  created: function () {
+    this.getData()
+  },
+
 }
+
+
 </script>
 
 <style scoped>
@@ -197,4 +259,128 @@ export default {
   margin-bottom: 0;
   text-decoration: none;
 }
+
+.Contenedor {
+  top: 400px;
+  left: 20px;
+  position: absolute;
+  width: auto;
+  height: auto;
+}
+
+.Columnas {
+  width: 1200px;
+  height: 50px;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+  box-shadow: 10px 10px 30px 0px rgba(0, 0, 0, 0.25);
+  box-sizing: border-box;
+  align-items: flex-start;
+  flex-shrink: 0;
+  border-color: var(--dl-color-default-defaultstroke);
+  /*border-style: solid;*/
+  border-width: 1px;
+  margin-right: 0;
+  border-radius: 15px;
+  margin-bottom: 0;
+  background-color: var(--dl-color-default-formbackground);
+  text-align: left;
+  font-family: Poppins;
+  flex-direction: column;
+  margin: 10px;
+}
+
+.Columna1 {
+  position: absolute;
+  left: 30px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+  
+}
+.Columna2 {
+  position: absolute;
+  left: 100px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+.Columna3 {
+  position: absolute;
+  left: 200px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+.Columna4 {
+  position: absolute;
+  left: 300px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+.Columna5 {
+  position: absolute;
+  left: 400px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+.Columna6 {
+  position: absolute;
+  left: 500px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+.Columna7 {
+  position: absolute;
+  left: 600px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+.Columna8 {
+  position: absolute;
+  left: 700px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+.Columna9 {
+  position: absolute;
+  left: 800px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+.Columna10 {
+  position: absolute;
+  left: 1070px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+
 </style>
--- pages/InventarioMarco.vue
+++ pages/InventarioMarco.vue
@@ -16,6 +16,29 @@
         </div>
       </nuxt-link>
       <span class="inventario-marco-text06"><span>Listado</span></span>
+
+      <div class="Contenedor">
+
+        <div class="Columnas">
+          <span class="Columna1">Modelo</span>
+          <span class="Columna2">Codigo Color</span>
+          <span class="Columna3">Alias del color</span>
+          <span class="Columna4">Estado del marco</span>
+          <span class="Columna5">Sucursal</span>
+        </div>
+
+        
+          <div class="Columnas" v-for="(marco, index) in listadoMarcos" :key="index">
+            <span class="Columna1">{{ marco.modelo}}</span>
+            <span class="Columna2">{{ marco.codigo_color }}</span>
+            <span class="Columna3">{{ marco.alias_color }}</span>
+            <span class="Columna4">{{ marco.estado_de_marco }}</span>
+            <span class="Columna5">Sucursal</span>
+          </div>
+        
+
+      </div>
+
       <div class="inventario-marco-descargar">
         <span class="inventario-marco-text08">
           <span>Descargar inventario para proovedores</span>
@@ -30,6 +53,29 @@ export default {
   name: 'InventarioMarco',
   head: {
     title: 'exported project',
+  },
+  
+  data: function () {
+    return {
+      listadoMarcos: [],
+    }
+  },
+
+  methods: {
+    getData: async function () {
+      try {
+
+        let response = await this.$axios.get('/marco')
+        this.listadoMarcos = response.data
+
+      } catch (error) {
+        console.log('Error al obtener listado marcos',error)
+      }
+    },
+  },
+
+  created: function () {
+    this.getData()
   },
 }
 </script>
@@ -194,5 +240,81 @@ export default {
   margin-right: 0;
   margin-bottom: 0;
   text-decoration: none;
+}
+
+.Contenedor {
+  top: 400px;
+  left: 20px;
+  position: absolute;
+  width: auto;
+  height: auto;
+}
+
+.Columnas {
+  width: 1200px;
+  height: 50px;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+  box-shadow: 10px 10px 30px 0px rgba(0, 0, 0, 0.25);
+  box-sizing: border-box;
+  align-items: flex-start;
+  flex-shrink: 0;
+  border-color: var(--dl-color-default-defaultstroke);
+  /*border-style: solid;*/
+  border-width: 1px;
+  margin-right: 0;
+  border-radius: 15px;
+  margin-bottom: 0;
+  background-color: var(--dl-color-default-formbackground);
+  text-align: left;
+  font-family: Poppins;
+  margin: 10px;
+}
+
+.Columna1 {
+  position: absolute;
+  left: 30px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+.Columna2 {
+  position: absolute;
+  left: 100px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+.Columna3 {
+  position: absolute;
+  left: 200px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+.Columna4 {
+  position: absolute;
+  left: 300px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
+}
+.Columna5 {
+  position: absolute;
+  left: 400px;
+  top: 10px;
+  width: auto;
+  height: auto;
+  font-size: 20px;
+  font-style: Bold;
 }
 </style>
